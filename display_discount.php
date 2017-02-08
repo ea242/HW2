378f07a3633a3ece781a65310ddce599522e1ad9
Eric Atempa <ea242@njit.edu>
2017-02-08 18:33:26 -0500

    Added Posts

--- display_discount.php
+++ display_discount.php
@@ -5,23 +5,28 @@
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
+<?php
+$product = $_POST["product_description"];
+$price = $_POST["list_price"];
+$disc = $_POST["discount_percent"];
+?>
     <main>
         <h1>This page is under construction</h1>
 
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $product; ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $price; ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $disc; ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $disc; ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo $disc; ?></span><br>
     </main>
 </body>
 </html>
